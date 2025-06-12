29ff7b2f957b350c4132df6e756e017ce34525cc
H.P.G.D Mewan <134765110+dilinamewan@users.noreply.github.com>
2025-06-12 11:04:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-// Smooth scroll for navigation
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    target.scrollIntoView({ behavior: "smooth" });
-  });
-});
+// Add any custom JavaScript here
+console.log("Portfolio Loaded");
